1f5ef6ca8ba2b1a291a1d76efeff6a5b9b6c5c9b
ok-tomo <ok.tmk13131@gmail.com>
2023-01-06 01:33:25 +0900

    change: twindの追加と余分ファイルの削除

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,19 +5,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Deno Calc with fresh</title>
       </Head>
       <div>
-        <img
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p>
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
         <Counter start={3} />
       </div>
     </>
